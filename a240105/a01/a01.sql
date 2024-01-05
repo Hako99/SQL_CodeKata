df5af60873830187a8860b3dfb30205a119136f1
mine <mineki94@naver.com>
2024-01-05 21:23:50 +0900

    1번문제

--- a240105/a01/a01.sql
+++ a240105/a01/a01.sql
@@ -1,7 +1,7 @@
 -- 동물 보호소에 들어온 동물 중,
 -- 이름이 있는 동물의 ID를 조회하는 SQL 문을 작성해주세요.
--- 단, ID는 오름차순 정렬되어야 합니다.
+-- 단, ID는 오름차순 정렬되어야 합니다.11
 
 SELECT ANIMAL_ID
 FROM ANIMAL_INS
-WHERE NAME is not NULL
+WHERE NAME is  NULL
