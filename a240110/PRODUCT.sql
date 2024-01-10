23a81353d3ef49ee0f677b940802081b40e23c8f
mine <mineki94@naver.com>
2024-01-10 21:37:53 +0900

    테이블 추가하기

---
+++ a240110/PRODUCT.sql
@@ -0,0 +1,67 @@
+create table PRODUCT
+(
+    product_id   number   not null,
+    product_code char(20) not null,
+    price        number   not null
+);
+
+insert into PRODUCT (product_id, product_code, price)
+values (1, 'C2000000', 72000);
+insert into PRODUCT (product_id, product_code, price)
+values (2, 'C1000000', 51000);
+insert into PRODUCT (product_id, product_code, price)
+values (3, 'D1000000', 63000);
+insert into PRODUCT (product_id, product_code, price)
+values (4, 'D3000000', 82000);
+insert into PRODUCT (product_id, product_code, price)
+values (5, 'B2000000', 42000);
+insert into PRODUCT (product_id, product_code, price)
+values (6, 'D3000001', 85000);
+insert into PRODUCT (product_id, product_code, price)
+values (7, 'C2000001', 31000);
+insert into PRODUCT (product_id, product_code, price)
+values (8, 'C3000000', 46000);
+insert into PRODUCT (product_id, product_code, price)
+values (9, 'A2000000', 54000);
+insert into PRODUCT (product_id, product_code, price)
+values (10, 'B2000001', 72000);
+insert into PRODUCT (product_id, product_code, price)
+values (11, 'C3000001', 61000);
+insert into PRODUCT (product_id, product_code, price)
+values (12, 'A3000000', 60000);
+insert into PRODUCT (product_id, product_code, price)
+values (13, 'C4000000', 54000);
+insert into PRODUCT (product_id, product_code, price)
+values (14, 'C3000002', 19000);
+insert into PRODUCT (product_id, product_code, price)
+values (15, 'D2000000', 35000);
+insert into PRODUCT (product_id, product_code, price)
+values (16, 'A3000001', 15000);
+insert into PRODUCT (product_id, product_code, price)
+values (17, 'D2000001', 46000);
+insert into PRODUCT (product_id, product_code, price)
+values (18, 'B1000000', 65000);
+insert into PRODUCT (product_id, product_code, price)
+values (19, 'D1000001', 62000);
+insert into PRODUCT (product_id, product_code, price)
+values (20, 'B2000002', 80000);
+insert into PRODUCT (product_id, product_code, price)
+values (21, 'A2000001', 42000);
+insert into PRODUCT (product_id, product_code, price)
+values (22, 'C3000003', 55000);
+insert into PRODUCT (product_id, product_code, price)
+values (23, 'C4000001', 65000);
+insert into PRODUCT (product_id, product_code, price)
+values (24, 'C4000002', 21000);
+insert into PRODUCT (product_id, product_code, price)
+values (25, 'A3000002', 32000);
+insert into PRODUCT (product_id, product_code, price)
+values (26, 'D1000002', 51000);
+insert into PRODUCT (product_id, product_code, price)
+values (27, 'D3000002', 60000);
+insert into PRODUCT (product_id, product_code, price)
+values (28, 'A3000003', 80000);
+insert into PRODUCT (product_id, product_code, price)
+values (29, 'C1000001', 51000);
+insert into PRODUCT (product_id, product_code, price)
+values (30, 'A2000002', 22000);
---
+++ a240110/USER_INFO.sql
@@ -0,0 +1,411 @@
+create table USER_INFO
+(
+    USER_ID number not null,
+    GENDER  number null,
+    age     number null,
+    joined  date not null
+);
+
+insert into USER_INFO (user_id, gender, age, joined)
+values (1, 1, 25, '2021-06-03');
+insert into USER_INFO (user_id, gender, age, joined)
+values (2, 1, 24, '2021-06-06');
+insert into USER_INFO (user_id, gender, age, joined)
+values (3, 1, 31, '2021-06-09');
+insert into USER_INFO (user_id, gender, age, joined)
+values (4, 1, 23, '2021-06-09');
+insert into USER_INFO (user_id, gender, age, joined)
+values (5, 1, 22, '2021-06-11');
+insert into USER_INFO (user_id, gender, age, joined)
+values (6, 0, 40, '2021-06-11');
+insert into USER_INFO (user_id, gender, age, joined)
+values (7, 1, 36, '2021-06-13');
+insert into USER_INFO (user_id, gender, age, joined)
+values (8, 1, 32, '2021-06-13');
+insert into USER_INFO (user_id, gender, age, joined)
+values (9, 0, 23, '2021-06-15');
+insert into USER_INFO (user_id, gender, age, joined)
+values (10, 0, 24, '2021-06-15');
+insert into USER_INFO (user_id, gender, age, joined)
+values (11, 1, 38, '2021-06-16');
+insert into USER_INFO (user_id, gender, age, joined)
+values (12, 0, 35, '2021-06-16');
+insert into USER_INFO (user_id, gender, age, joined)
+values (13, 1, 31, '2021-06-17');
+insert into USER_INFO (user_id, gender, age, joined)
+values (14, 1, 34, '2021-06-20');
+insert into USER_INFO (user_id, gender, age, joined)
+values (15, 0, 29, '2021-06-20');
+insert into USER_INFO (user_id, gender, age, joined)
+values (16, 0, 25, '2021-06-22');
+insert into USER_INFO (user_id, gender, age, joined)
+values (17, 0, 27, '2021-06-23');
+insert into USER_INFO (user_id, gender, age, joined)
+values (18, 1, 29, '2021-06-26');
+insert into USER_INFO (user_id, gender, age, joined)
+values (19, 1, 26, '2021-06-26');
+insert into USER_INFO (user_id, gender, age, joined)
+values (20, 0, 29, '2021-06-29');
+insert into USER_INFO (user_id, gender, age, joined)
+values (21, 0, 29, '2021-07-01');
+insert into USER_INFO (user_id, gender, age, joined)
+values (22, 0, 29, '2021-07-01');
+insert into USER_INFO (user_id, gender, age, joined)
+values (23, 1, 33, '2021-07-04');
+insert into USER_INFO (user_id, gender, age, joined)
+values (24, 0, 24, '2021-07-06');
+insert into USER_INFO (user_id, gender, age, joined)
+values (25, 1, 30, '2021-07-06');
+insert into USER_INFO (user_id, gender, age, joined)
+values (26, 1, 29, '2021-07-08');
+insert into USER_INFO (user_id, gender, age, joined)
+values (27, 1, 22, '2021-07-09');
+insert into USER_INFO (user_id, gender, age, joined)
+values (28, 1, 21, '2021-07-10');
+insert into USER_INFO (user_id, gender, age, joined)
+values (29, 1, 30, '2021-07-10');
+insert into USER_INFO (user_id, gender, age, joined)
+values (30, 1, 20, '2021-07-11');
+insert into USER_INFO (user_id,  age, joined)
+values (31,  25, '2021-07-11');
+insert into USER_INFO (user_id, gender, age, joined)
+values (32, 1, 44, '2021-07-14');
+insert into USER_INFO (user_id, gender, age, joined)
+values (33, 1, 24, '2021-07-14');
+insert into USER_INFO (user_id, gender, age, joined)
+values (34, 1, 24, '2021-07-17');
+insert into USER_INFO (user_id, gender, age, joined)
+values (35, 1, 37, '2021-07-18');
+insert into USER_INFO (user_id, gender, age, joined)
+values (36, 1, 24, '2021-07-21');
+insert into USER_INFO (user_id, gender,  joined)
+values (37, 0 , '2021-07-22');
+insert into USER_INFO (user_id, gender, age, joined)
+values (38, 0, 24, '2021-07-24');
+insert into USER_INFO (user_id, gender, age, joined)
+values (39, 0, 26, '2021-07-24');
+insert into USER_INFO (user_id, gender, age, joined)
+values (40, 1, 23, '2021-07-27');
+insert into USER_INFO (user_id, gender, age, joined)
+values (41, 1, 29, '2021-07-28');
+insert into USER_INFO (user_id, gender, age, joined)
+values (42, 0, 40, '2021-07-28');
+insert into USER_INFO (user_id, gender, age, joined)
+values (43, 1, 23, '2021-07-31');
+insert into USER_INFO (user_id, gender, age, joined)
+values (44, 1, 41, '2021-08-01');
+insert into USER_INFO (user_id, gender, age, joined)
+values (45, 1, 41, '2021-08-01');
+insert into USER_INFO (user_id, gender, age, joined)
+values (46, 0, 39, '2021-08-03');
+insert into USER_INFO (user_id, gender, age, joined)
+values (47, 1, 24, '2021-08-03');
+insert into USER_INFO (user_id, gender, age, joined)
+values (48, 1, 24, '2021-08-05');
+insert into USER_INFO (user_id, gender, age, joined)
+values (49, 1, 20, '2021-08-07');
+insert into USER_INFO (user_id, gender, age, joined)
+values (50, 1, 28, '2021-08-08');
+insert into USER_INFO (user_id, gender, age, joined)
+values (51, 1, 37, '2021-08-08');
+insert into USER_INFO (user_id, gender, age, joined)
+values (52, 0, 44, '2021-08-09');
+insert into USER_INFO (user_id, gender, age, joined)
+values (53, 0, 25, '2021-08-09');
+insert into USER_INFO (user_id, gender, age, joined)
+values (54, 0, 25, '2021-08-12');
+insert into USER_INFO (user_id, gender, age, joined)
+values (55, 1, 25, '2021-08-14');
+insert into USER_INFO (user_id, gender, age, joined)
+values (56, 1, 40, '2021-08-14');
+insert into USER_INFO (user_id, gender, age, joined)
+values (57, 1, 27, '2021-08-17');
+insert into USER_INFO (user_id, gender, age, joined)
+values (58, 1, 41, '2021-08-17');
+insert into USER_INFO (user_id, gender, age, joined)
+values (59, 0, 21, '2021-08-19');
+insert into USER_INFO (user_id,  age, joined)
+values (60 , 20, '2021-08-19');
+insert into USER_INFO (user_id, gender, age, joined)
+values (61, 1, 29, '2021-08-21');
+insert into USER_INFO (user_id, gender, age, joined)
+values (62, 1, 25, '2021-08-24');
+insert into USER_INFO (user_id, gender, age, joined)
+values (63, 0, 20, '2021-08-24');
+insert into USER_INFO (user_id, gender, age, joined)
+values (64, 0, 29, '2021-08-25');
+insert into USER_INFO (user_id, gender, age, joined)
+values (65, 1, 29, '2021-08-27');
+insert into USER_INFO (user_id, gender, age, joined)
+values (66, 0, 27, '2021-08-27');
+insert into USER_INFO (user_id, gender, age, joined)
+values (67, 1, 20, '2021-08-29');
+insert into USER_INFO (user_id, gender, age, joined)
+values (68, 0, 20, '2021-08-30');
+insert into USER_INFO (user_id, gender, age, joined)
+values (69, 0, 33, '2021-08-30');
+insert into USER_INFO (user_id, gender, age, joined)
+values (70, 1, 27, '2021-09-01');
+insert into USER_INFO (user_id, gender, age, joined)
+values (71, 1, 36, '2021-09-01');
+insert into USER_INFO (user_id, gender, age, joined)
+values (72, 1, 33, '2021-09-04');
+insert into USER_INFO (user_id, gender, age, joined)
+values (73, 0, 26, '2021-09-06');
+insert into USER_INFO (user_id, gender, age, joined)
+values (74, 1, 27, '2021-09-07');
+insert into USER_INFO (user_id, gender, age, joined)
+values (75, 0, 26, '2021-09-07');
+insert into USER_INFO (user_id, gender, age, joined)
+values (76, 1, 36, '2021-09-09');
+insert into USER_INFO (user_id, gender, age, joined)
+values (77, 1, 30, '2021-09-12');
+insert into USER_INFO (user_id, gender, age, joined)
+values (78, 0, 21, '2021-09-13');
+insert into USER_INFO (user_id, gender, age, joined)
+values (79, 1, 32, '2021-09-15');
+insert into USER_INFO (user_id, gender, age, joined)
+values (80, 0, 30, '2021-09-15');
+insert into USER_INFO (user_id, gender, age, joined)
+values (81, 0, 38, '2021-09-16');
+insert into USER_INFO (user_id, gender, age, joined)
+values (82, 1, 27, '2021-09-16');
+insert into USER_INFO (user_id, gender, age, joined)
+values (83, 1, 34, '2021-09-19');
+insert into USER_INFO (user_id, gender, age, joined)
+values (84, 1, 33, '2021-09-22');
+insert into USER_INFO (user_id, gender, age, joined)
+values (85, 0, 27, '2021-09-22');
+insert into USER_INFO (user_id, gender, age, joined)
+values (86, 0, 28, '2021-09-25');
+insert into USER_INFO (user_id, gender, age, joined)
+values (87, 1, 38, '2021-09-28');
+insert into USER_INFO (user_id, gender, age, joined)
+values (88, 0, 30, '2021-09-28');
+insert into USER_INFO (user_id, gender, age, joined)
+values (89, 1, 24, '2021-10-01');
+insert into USER_INFO (user_id, gender, age, joined)
+values (90, 0, 20, '2021-10-01');
+insert into USER_INFO (user_id, gender, age, joined)
+values (91, 0, 24, '2021-10-02');
+insert into USER_INFO (user_id, gender, age, joined)
+values (92, 0, 21, '2021-10-02');
+insert into USER_INFO (user_id, gender, age, joined)
+values (93, 1, 34, '2021-10-03');
+insert into USER_INFO (user_id, gender, age, joined)
+values (94, 0, 43, '2021-10-03');
+insert into USER_INFO (user_id, gender, age, joined)
+values (95, 0, 22, '2021-10-04');
+insert into USER_INFO (user_id, gender, age, joined)
+values (96, 1, 21, '2021-10-07');
+insert into USER_INFO (user_id, gender, age, joined)
+values (97, 0, 35, '2021-10-10');
+insert into USER_INFO (user_id, gender, age, joined)
+values (98, 1, 20, '2021-10-10');
+insert into USER_INFO (user_id, gender, age, joined)
+values (99, 0, 30, '2021-10-11');
+insert into USER_INFO (user_id, gender, age, joined)
+values (100, 0, 41, '2021-10-11');
+insert into USER_INFO (user_id, gender, age, joined)
+values (101, 1, 31, '2021-10-12');
+insert into USER_INFO (user_id, gender, age, joined)
+values (102, 0, 29, '2021-10-12');
+insert into USER_INFO (user_id, gender, age, joined)
+values (103, 0, 23, '2021-10-15');
+insert into USER_INFO (user_id, gender, age, joined)
+values (104, 0, 38, '2021-10-15');
+insert into USER_INFO (user_id, gender, age, joined)
+values (105, 0, 21, '2021-10-17');
+insert into USER_INFO (user_id, gender, age, joined)
+values (106, 1, 20, '2021-10-17');
+insert into USER_INFO (user_id, gender, age, joined)
+values (107, 1, 36, '2021-10-20');
+insert into USER_INFO (user_id, gender, age, joined)
+values (108, 1, 24, '2021-10-20');
+insert into USER_INFO (user_id, gender, age, joined)
+values (109, 1, 23, '2021-10-22');
+insert into USER_INFO (user_id, gender,  joined)
+values (110, 1 , '2021-10-24');
+insert into USER_INFO (user_id, gender, age, joined)
+values (111, 0, 37, '2021-10-26');
+insert into USER_INFO (user_id, gender, age, joined)
+values (112, 1, 24, '2021-10-26');
+insert into USER_INFO (user_id, gender, age, joined)
+values (113, 0, 32, '2021-10-28');
+insert into USER_INFO (user_id, gender, age, joined)
+values (114, 0, 37, '2021-10-31');
+insert into USER_INFO (user_id, gender, age, joined)
+values (115, 1, 24, '2021-10-31');
+insert into USER_INFO (user_id, gender, age, joined)
+values (116, 1, 28, '2021-11-02');
+insert into USER_INFO (user_id, gender, age, joined)
+values (117, 0, 25, '2021-11-02');
+insert into USER_INFO (user_id, gender, age, joined)
+values (118, 1, 26, '2021-11-05');
+insert into USER_INFO (user_id, gender, age, joined)
+values (119, 1, 30, '2021-11-06');
+insert into USER_INFO (user_id, gender, age, joined)
+values (120, 0, 38, '2021-11-06');
+insert into USER_INFO (user_id, gender, age, joined)
+values (121, 0, 29, '2021-11-09');
+insert into USER_INFO (user_id, gender, age, joined)
+values (122, 1, 23, '2021-11-09');
+insert into USER_INFO (user_id, gender, age, joined)
+values (123, 0, 23, '2021-11-11');
+insert into USER_INFO (user_id, gender, age, joined)
+values (124, 1, 27, '2021-11-11');
+insert into USER_INFO (user_id, gender, age, joined)
+values (125, 0, 22, '2021-11-12');
+insert into USER_INFO (user_id, gender, age, joined)
+values (126, 0, 31, '2021-11-14');
+insert into USER_INFO (user_id, gender, age, joined)
+values (127, 1, 24, '2021-11-17');
+insert into USER_INFO (user_id, gender, age, joined)
+values (128, 1, 23, '2021-11-20');
+insert into USER_INFO (user_id, gender, age, joined)
+values (129, 0, 21, '2021-11-20');
+insert into USER_INFO (user_id, gender, age, joined)
+values (130, 1, 23, '2021-11-21');
+insert into USER_INFO (user_id, gender, age, joined)
+values (131, 1, 29, '2021-11-21');
+insert into USER_INFO (user_id, gender, age, joined)
+values (132, 1, 22, '2021-11-23');
+insert into USER_INFO (user_id, gender, age, joined)
+values (133, 1, 22, '2021-11-26');
+insert into USER_INFO (user_id, gender, age, joined)
+values (134, 0, 27, '2021-11-26');
+insert into USER_INFO (user_id, gender, age, joined)
+values (135, 1, 40, '2021-11-29');
+insert into USER_INFO (user_id, gender, age, joined)
+values (136, 0, 33, '2021-11-30');
+insert into USER_INFO (user_id, gender, age, joined)
+values (137, 1, 22, '2021-12-01');
+insert into USER_INFO (user_id, gender, age, joined)
+values (138, 0, 41, '2021-12-01');
+insert into USER_INFO (user_id, gender, age, joined)
+values (139, 0, 37, '2021-12-04');
+insert into USER_INFO (user_id, gender, age, joined)
+values (140, 1, 23, '2021-12-06');
+insert into USER_INFO (user_id, gender, age, joined)
+values (141, 1, 29, '2021-12-06');
+insert into USER_INFO (user_id, gender, age, joined)
+values (142, 1, 40, '2021-12-09');
+insert into USER_INFO (user_id, gender, age, joined)
+values (143, 0, 25, '2021-12-12');
+insert into USER_INFO (user_id, gender, age, joined)
+values (144, 1, 23, '2021-12-13');
+insert into USER_INFO (user_id, gender, age, joined)
+values (145, 0, 26, '2021-12-16');
+insert into USER_INFO (user_id, gender, age, joined)
+values (146, 1, 42, '2021-12-16');
+insert into USER_INFO (user_id, gender, age, joined)
+values (147, 1, 22, '2021-12-18');
+insert into USER_INFO (user_id, gender, age, joined)
+values (148, 1, 27, '2021-12-18');
+insert into USER_INFO (user_id, gender, age, joined)
+values (149, 1, 33, '2021-12-19');
+insert into USER_INFO (user_id, gender, age, joined)
+values (150, 1, 26, '2021-12-22');
+insert into USER_INFO (user_id, gender, age, joined)
+values (151, 1, 33, '2021-12-25');
+insert into USER_INFO (user_id, gender, age, joined)
+values (152, 0, 25, '2021-12-25');
+insert into USER_INFO (user_id, gender, age, joined)
+values (153, 0, 25, '2021-12-26');
+insert into USER_INFO (user_id, gender,  joined)
+values (154, 0 , '2021-12-26');
+insert into USER_INFO (user_id, gender, age, joined)
+values (155, 1, 36, '2021-12-27');
+insert into USER_INFO (user_id, gender,  joined)
+values (156, 1 , '2021-12-27');
+insert into USER_INFO (user_id, gender, age, joined)
+values (157, 0, 22, '2021-12-30');
+insert into USER_INFO (user_id, gender, age, joined)
+values (158, 1, 30, '2021-12-30');
+insert into USER_INFO (user_id, gender, age, joined)
+values (159, 0, 22, '2022-01-02');
+insert into USER_INFO (user_id, gender, age, joined)
+values (160, 0, 33, '2022-01-03');
+insert into USER_INFO (user_id, gender, age, joined)
+values (161, 0, 32, '2022-01-05');
+insert into USER_INFO (user_id, gender, age, joined)
+values (162, 0, 28, '2022-01-07');
+insert into USER_INFO (user_id, gender, age, joined)
+values (163, 1, 36, '2022-01-07');
+insert into USER_INFO (user_id, gender, age, joined)
+values (164, 1, 22, '2022-01-08');
+insert into USER_INFO (user_id, gender, age, joined)
+values (165, 1, 24, '2022-01-10');
+insert into USER_INFO (user_id, gender, age, joined)
+values (166, 1, 39, '2022-01-10');
+insert into USER_INFO (user_id,  age, joined)
+values (167 , 42, '2022-01-13');
+insert into USER_INFO (user_id, gender, age, joined)
+values (168, 0, 32, '2022-01-16');
+insert into USER_INFO (user_id,  age, joined)
+values (169 , 28, '2022-01-19');
+insert into USER_INFO (user_id, gender, age, joined)
+values (170, 1, 25, '2022-01-20');
+insert into USER_INFO (user_id, gender, age, joined)
+values (171, 1, 21, '2022-01-20');
+insert into USER_INFO (user_id, gender, age, joined)
+values (172, 0, 21, '2022-01-22');
+insert into USER_INFO (user_id, gender, age, joined)
+values (173, 0, 21, '2022-01-22');
+insert into USER_INFO (user_id, gender, age, joined)
+values (174, 1, 26, '2022-01-25');
+insert into USER_INFO (user_id, gender, age, joined)
+values (175, 0, 21, '2022-01-27');
+insert into USER_INFO (user_id, gender, age, joined)
+values (176, 1, 40, '2022-01-27');
+insert into USER_INFO (user_id, gender, age, joined)
+values (177, 1, 24, '2022-01-30');
+insert into USER_INFO (user_id, gender, age, joined)
+values (178, 1, 43, '2022-01-30');
+insert into USER_INFO (user_id, gender, age, joined)
+values (179, 1, 31, '2022-01-31');
+insert into USER_INFO (user_id, gender, age, joined)
+values (180, 0, 20, '2022-02-03');
+insert into USER_INFO (user_id, gender, age, joined)
+values (181, 1, 21, '2022-02-03');
+insert into USER_INFO (user_id, gender, age, joined)
+values (182, 0, 34, '2022-02-05');
+insert into USER_INFO (user_id, gender, age, joined)
+values (183, 1, 25, '2022-02-06');
+insert into USER_INFO (user_id,  age, joined)
+values (184 , 21, '2022-02-09');
+insert into USER_INFO (user_id, gender, age, joined)
+values (185, 0, 32, '2022-02-11');
+insert into USER_INFO (user_id, gender, age, joined)
+values (186, 1, 20, '2022-02-13');
+insert into USER_INFO (user_id, gender, age, joined)
+values (187, 1, 34, '2022-02-14');
+insert into USER_INFO (user_id, gender, age, joined)
+values (188, 0, 23, '2022-02-14');
+insert into USER_INFO (user_id, gender, age, joined)
+values (189, 0, 36, '2022-02-15');
+insert into USER_INFO (user_id, gender, age, joined)
+values (190, 1, 24, '2022-02-15');
+insert into USER_INFO (user_id, gender, age, joined)
+values (191, 0, 32, '2022-02-17');
+insert into USER_INFO (user_id, gender, age, joined)
+values (192, 0, 28, '2022-02-17');
+insert into USER_INFO (user_id, gender, age, joined)
+values (193, 1, 34, '2022-02-19');
+insert into USER_INFO (user_id, gender, age, joined)
+values (194, 1, 27, '2022-02-21');
+insert into USER_INFO (user_id, gender, age, joined)
+values (195, 1, 24, '2022-02-24');
+insert into USER_INFO (user_id, gender, age, joined)
+values (196, 0, 30, '2022-02-24');
+insert into USER_INFO (user_id, gender, age, joined)
+values (197, 0, 39, '2022-02-27');
+insert into USER_INFO (user_id, gender, age, joined)
+values (198, 1, 43, '2022-03-02');
+insert into USER_INFO (user_id, gender, age, joined)
+values (199, 0, 28, '2022-03-03');
+insert into USER_INFO (user_id, gender, age, joined)
+values (200, 0, 21, '2022-03-03');
+
+select *
+from USER_INFO
