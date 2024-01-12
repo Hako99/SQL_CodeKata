3978bf51dbb1665bdf300daf58a016f4ab56e38b
mine <mineki94@naver.com>
2024-01-12 21:17:07 +0900

    20번 문제

---
+++ a240112/a20/a20.sql
@@ -0,0 +1,9 @@
+-- FOOD_PRODUCT 테이블에서 가격이 제일 비싼 식품의
+-- 식품 ID, 식품 이름, 식품 코드, 식품분류, 식품 가격을
+-- 조회하는 SQL문을 작성해주세요.
+
+-- https://school.programmers.co.kr/learn/courses/30/lessons/131115?language=mysql
+
+SELECT product_id,product_name,product_cd,category,price
+from food_product
+where price = (select max(price) from food_product)
