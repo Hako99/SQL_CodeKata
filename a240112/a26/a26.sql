25ec6de9e3e33b8299442162d471be11e4134c61
mine <mineki94@naver.com>
2024-01-12 21:17:26 +0900

    26번 문제

---
+++ a240112/a26/a26.sql
@@ -0,0 +1,22 @@
+-- 보호소에서는 몇 시에 입양이 가장 활발하게 일어나는지 알아보려 합니다.
+-- 09:00부터 19:59까지, 각 시간대별로
+-- 입양이 몇 건이나 발생했는지 조회하는 SQL문을 작성해주세요.
+-- 이때 결과는 시간대 순으로 정렬해야 합니다.
+
+-- https://school.programmers.co.kr/learn/courses/30/lessons/59412
+
+
+
+SELECT extract(hour from datetime) hour , count(datetime) as count
+from animal_outs
+group by extract(hour from datetime)
+having extract(hour from datetime)>= 9 and extract(hour from datetime) <= 19
+order by hour asc
+
+/*
+ SELECT hour(datetime) hour , count(datetime) as count
+from animal_outs
+group by hour(datetime)
+having hour >= 9 and hour <= 19
+order by hour asc
+ */
