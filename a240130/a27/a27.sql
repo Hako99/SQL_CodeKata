edf1406dcec08d701910fe2a30e9d7350da38bbd
mine <mineki94@naver.com>
2024-01-30 20:57:17 +0900

    27번 문제

---
+++ a240130/a27/a27.sql
@@ -0,0 +1,21 @@
+--문제 APPOINTMENT 테이블에서 2022년 5월에 예약한 환자 수를 진료과코드 별로 조회하는 SQL문을 작성해주세요.
+-- 이때, 컬럼명은 '진료과 코드', '5월예약건수'로 지정해주시고
+-- 결과는 진료과별 예약한 환자 수를 기준으로 오름차순 정렬하고,
+-- 예약한 환자 수가 같다면 진료과 코드를 기준으로 오름차순 정렬해주세요.
+
+--https://school.programmers.co.kr/learn/courses/30/lessons/132202?language=oracle
+
+
+SELECT MCDP_CD "진료과코드", COUNT (MCDP_CD) "5월예약건수"
+FROM APPOINTMENT
+WHERE TO_CHAR(APNT_YMD, 'MM') =5
+GROUP BY MCDP_CD
+ORDER BY COUNT(MCDP_CD) ASC, MCDP_CD ASC
+
+/*
+ SELECT MCDP_CD "진료과코드", COUNT (MCDP_CD) "5월예약건수"
+FROM APPOINTMENT
+WHERE APNT_YMD LIKE '2022-05%'
+GROUP BY MCDP_CD
+ORDER BY 5월예약건수 ASC, MCDP_CD ASC
+ */
